0d908f8ff86ba0f3463698faf61fc5c3274b384c
Ryan Schachte <schachte@cloudflare.com>
2022-12-29 11:04:39 -0800

    Updating tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  purge: ["./components//*.{js,ts,jsx,tsx}", "./pages//*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
